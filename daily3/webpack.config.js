eb970291d019f462b8bdfd1daf58c590dc9d6dab
yhl <yinhengli@sina.com>
2017-07-10 22:21:09 +0800

    output-management

---
+++ daily3/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    "file-loader"
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+
+}
